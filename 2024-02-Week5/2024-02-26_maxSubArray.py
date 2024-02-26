8759e8a56b40cdaf5d532ba076a8ab9181927b8e
byj1009 <byj920501@naver.com>
2024-02-26 23:37:20 +0900

    2024-02-26_변영준

---
+++ 2024-02-Week5/2024-02-26_maxSubArray.py
@@ -0,0 +1,43 @@
+"""
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+"""
+
+# 풀이 1 // Time limit 초과
+class Solution:
+    def maxSubArray(self, nums):
+        ans = -inf
+        for i in range(len(nums)):
+            cur_sum = 0
+            for j in range(i, len(nums)):
+                cur_sum += nums[j]
+                ans = max(ans, cur_sum)
+        return ans
+
+
+# 풀이 2
+class Solution:
+    def maxSubArray(self, nums):
+        dp = [[0]*len(nums) for i in range(2)]
+        dp[0][0], dp[1][0] = nums[0], nums[0]
+        for i in range(1, len(nums)):
+            dp[1][i] = max(nums[i], nums[i] + dp[1][i-1])
+            dp[0][i] = max(dp[0][i-1], dp[1][i])
+        return dp[0][-1]
