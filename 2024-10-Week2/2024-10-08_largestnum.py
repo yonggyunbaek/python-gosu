a92584104497c1444b69695d8d0022dba8c0c2c7
Youngjun <76712506+byj1009@users.noreply.github.com>
2024-10-08 21:30:01 +0900

    2024-10-08_변영준

---
+++ 2024-10-Week2/2024-10-08_largestnum.py
@@ -0,0 +1,17 @@
+# 
+
+def solution(number, k):
+    stack = []
+
+    for i in range(len(number)):
+        while k > 0 and stack and stack[-1] < number[i]:
+            stack.pop()
+            k -= 1
+        stack.append(number[i])
+    
+
+    if k > 0:
+        stack = stack[:-k]
+    
+
+    return ''.join(stack)
