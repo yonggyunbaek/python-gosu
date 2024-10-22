d86f986d1cb39575d759bcb4e9088c7bb2cf8b0a
Youngjun <76712506+byj1009@users.noreply.github.com>
2024-10-22 10:52:54 +0900

    2024-10-22_변영준

---
+++ 2024-10-Week4/2024-10-22_numIdenticalPairs.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/number-of-good-pairs/submissions/
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        ans = 0
+        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    ans += 1
+                    # print(i,j)
+        return ans
