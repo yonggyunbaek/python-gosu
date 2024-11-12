b4d988e21e16d8a0d428d4eac0535727710d924e
Youngjun <76712506+byj1009@users.noreply.github.com>
2024-11-12 22:23:05 +0900

    2024-11-12_변영준

---
+++ 2024-11-Week2/2024-11-12_basic4.java
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120807
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if(num1==num2){
+			answer = 1;
+		}
+        else{
+            answer = -1;
+        }
+        return answer;
+    }
+}
