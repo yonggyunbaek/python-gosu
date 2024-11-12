47ba1a9c963dcb6555d6a6f53dd8684a266e50c2
Youngjun <76712506+byj1009@users.noreply.github.com>
2024-11-12 22:19:41 +0900

    2024-11-12_변영준

---
+++ 2024-11-Week2/2024-11-12_basic3.java
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+}
