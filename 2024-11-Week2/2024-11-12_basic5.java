e1b121c6ef99154285b34908f5c48e8a1eae19e6
Youngjun <76712506+byj1009@users.noreply.github.com>
2024-11-12 22:32:11 +0900

    2024-11-12_변영준

---
+++ 2024-11-Week2/2024-11-12_basic5.java
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120820
+
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        answer = 2022 - age + 1;
+        return answer;
+    }
+}
