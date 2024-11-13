d4ace9c59ecfc0dbb73e0766e254b0f5c90a8e18
Youngjun <76712506+byj1009@users.noreply.github.com>
2024-11-13 21:46:30 +0900

    2024-11-13_변영준

---
+++ 2024-11-Week2/2024-11-13_basic1.java
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120806
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = (int) ((double) num1 / num2 * 1000);
+        return answer;
+    }
+}
