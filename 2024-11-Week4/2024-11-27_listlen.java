b8ec440634aa2e173ed68eb65c4a2b63a6e1dab9
Youngjun <76712506+byj1009@users.noreply.github.com>
2024-11-27 19:58:04 +0900

    2024-11-27_변영준

---
+++ 2024-11-Week4/2024-11-27_listlen.java
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120854
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        
+        for(int i=0; i < strlist.length; i++){
+            answer[i] = strlist[i].length();
+        }
+        
+        return answer;
+    }
+}
