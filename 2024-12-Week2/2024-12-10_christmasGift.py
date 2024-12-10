4b8623ed846dd8ff42606da1ac8724edf12e4623
yonggyunbaek <dydrbs159@gmail.com>
2024-12-10 23:08:39 +0900

    2024-12-10_백용균

---
+++ 2024-12-Week2/2024-12-10_christmasGift.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/14235/
+
+
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+gift = []
+for i in range(n):
+    nums = list(map(int, input().split()))
+    a = nums[0]    
+    if a == 0 and len(gift) == 0:
+        print(-1)
+    elif a == 0 and len(gift) != 0:
+        print(heapq.heappop(gift)*-1)
+    else:
+        for num in nums[1:]:
+            heapq.heappush(gift, -num)  # 음수로 넣어서 최소 힙을 사용
