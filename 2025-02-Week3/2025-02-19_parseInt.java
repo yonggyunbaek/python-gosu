a812055d04f9a7302fa8fd2d8cc0e69f44657a10
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-02-19 22:35:29 +0900

    2025-02-19_변영준

---
+++ 2025-02-Week3/2025-02-19_parseInt.java
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120885
+
+class Solution {
+    public String solution(String bin1, String bin2) {
+        int num1 = Integer.parseInt(bin1, 2);
+        int num2 = Integer.parseInt(bin2, 2);
+        
+        int sum = num1 + num2;
+        
+        return Integer.toBinaryString(sum);
+    }
+}
