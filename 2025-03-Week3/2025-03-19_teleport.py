847be2124b9fb3cc543d505da4f554607dc30f48
yonggyunbaek <dydrbs159@gmail.com>
2025-03-20 21:53:07 +0900

    2025-03-20_백용균(teleport.py 2/2)

--- 2025-03-Week3/2025-03-19_teleport.py
+++ 2025-03-Week3/2025-03-19_teleport.py
@@ -1,12 +1,18 @@
-# https://www.acmicpc.net/problem/16958
-
 from collections import defaultdict
 import sys
 
 input = sys.stdin.readline
 
-def distance(a,b):
-    return abs(cities[a][0] - cities[b][0]) + abs(cities[a][1] - cities[b][1])
+def distance(c1,c2):
+    return abs(cities[c1][0] - cities[c2][0]) + abs(cities[c1][1] - cities[c2][1])
+
+def findCloseTeleDistance(city):
+    min_d = 10000
+    for i in range(1,n+1):
+        if i != city:
+            if cities[i][2] == 1:
+                min_d = min(min_d,distance(city, i))
+    return min_d   
 
 n, t = map(int,input().split())
 cities = defaultdict(list)
@@ -20,33 +26,21 @@ m = int(input())
 for _ in range(m):
     a, b = map(int,input().split())
     # 두 도시 모두 텔포 안될경우
+    # 그냥 거리계산, 텔포가능한 도시 중간에 2개 껴서 가기 중 짧은 것
     if cities[a][2] == 0 and cities[b][2] == 0:
-        print(distance(a,b))
+        print(min(distance(a,b), findCloseTeleDistance(a)+t+findCloseTeleDistance(b)))
         continue
     # 시작점만 텔포 가능하면
     elif cities[a][2] == 1 and cities[b][2] == 0:
         # 도착점에서 가장 가까운 텔포 가능 도시 찾아
-        possible = {}
-        for i in range(1,n+1):
-            if i not in [a,b]:
-                if cities[i][2] == 1:
-                    possible[distance(b, i)] = i
-        mid_city = possible[min(possible.keys())]
-        print(min([(distance(a,b)), t+distance(b,mid_city)]))
+        print(min(findCloseTeleDistance(b)+t,distance(a,b)))
         continue
     # 도착점만 텔포 가능하면
     elif cities[a][2] == 0 and cities[b][2] == 1:
-        # 도착점에서 가장 가까운 텔포 가능 도시 찾아
-        possible = {}
-        for i in range(1,n+1):
-            if i not in [a,b]:
-                if cities[i][2] == 1:
-                    possible[distance(a, i)] = i
-        mid_city = possible[min(possible.keys())]
-        print(min([distance(a,b), t+distance(a,mid_city)]))
+        # 시작점에서 가장 가까운 텔포 가능 도시 찾아
+        print(min(findCloseTeleDistance(a)+t,distance(a,b)))
         continue
     # 시작 도착 둘다 텔포 가능이면
     else:
         print(min(distance(a,b), t))
                     
-
