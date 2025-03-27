11c26e13e5c1c067fc75da4433cb235d520a33c5
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-03-27 21:13:34 +0900

    2025-03-27_변영준

---
+++ 2025-03-Week4/2025-03-27_commonmultiple.java
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181936
+
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if(number%n==0 && number%m==0){
+            return 1;
+        }
+        return answer;
+    }
+}
