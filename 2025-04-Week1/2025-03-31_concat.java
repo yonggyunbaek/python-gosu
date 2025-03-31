05fc0649a97a07ba33c56ec22a8b9b3a972515f3
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-03-31 21:55:14 +0900

    2025-03-31_변영준

---
+++ 2025-04-Week1/2025-03-31_concat.java
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181946
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String answer = a.concat(b);
+        System.out.println(answer);
+    }
+}
