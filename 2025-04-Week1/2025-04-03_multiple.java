cfb6b0cbef28af3b013069e5609efd01dc363d22
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-04-03 20:21:03 +0900

    2025-04-03_변영준

---
+++ 2025-04-Week1/2025-04-03_multiple.java
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181937
+class Solution {
+    public int solution(int num, int n) {
+        int answer = 0;
+        if(num%n==0){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
