57900574bc9f0df4229887fc4418c63c607dc38e
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-04-08 22:19:19 +0900

    2025-04-08_변영준

---
+++ 2025-04-Week2/2025-04-08_contains2.java
@@ -0,0 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181843
+class Solution {
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        if(my_string.contains(target)){
+            return 1;
+        }
+        return 0;
+    }
+}
