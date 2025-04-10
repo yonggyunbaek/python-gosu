96d535debd5a9acca1957e691be77eadbce9c207
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-04-10 22:34:13 +0900

    2025-04-10_변영준

---
+++ 2025-04-Week2/2025-04-10_inttostring.java
@@ -0,0 +1,8 @@
+#  https://school.programmers.co.kr/learn/courses/30/lessons/181845
+class Solution {
+    public String solution(int n) {
+        String answer = "" + n;
+        
+        return answer;
+    }
+}
