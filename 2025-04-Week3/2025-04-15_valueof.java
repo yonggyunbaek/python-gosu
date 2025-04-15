55665d6be23ea1ee86a900c0d907c9ca4c223546
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-04-15 20:07:41 +0900

    2025-04-15_변영준

---
+++ 2025-04-Week3/2025-04-15_valueof.java
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181848
+
+class Solution {
+    public int solution(String n_str) {
+        int answer = Integer.valueOf(n_str);
+        return answer;
+    }
+}
