1d4f1f1527cd52efaeee63d1249f68a9fcb46ec8
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-05-07 22:05:49 +0900

    2025-05-07_변영준

---
+++ 2025-05-Week1/2025-05-07_Stringbuilder2.java
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181941
+
+class Solution {
+    public String solution(String[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for(String a:arr){
+            sb.append(a);
+        }
+        
+        return sb.toString();
+    }
+}
