28751164948468093a8e46008b70203838abe64c
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-05-09 21:30:44 +0900

    2025-05-09_변영준

---
+++ 2025-05-Week1/2025-05-09_trim-split2.java
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181869
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.trim().split("\\s+");
+        return answer;
+    }
+}
