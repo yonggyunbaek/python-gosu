89dad24bf95f3c3d53a332508f1072d766f86a64
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-05-12 23:52:19 +0900

    2025-05-12_변영준

---
+++ 2025-05-Week2/2025-05-12_upperlower.java
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181875
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        String[] answer = new String[strArr.length];
+        for(int i=0;i<strArr.length;i++){
+            if(i%2==1){
+                answer[i]=strArr[i].toUpperCase();
+            }
+            else{
+                answer[i]=strArr[i].toLowerCase();
+            }
+        }
+            
+        return answer;
+    }
+}
