a091b4a4c8e0b52b09b4e8e511eeaaebbb136cdd
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-05-13 22:42:20 +0900

    2025-05-13_변영준

---
+++ 2025-05-Week2/2025-05-13_tolowercase.java
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181876
+
+class Solution {
+    public String solution(String myString) {
+        return myString.toLowerCase();
+    }
+}
