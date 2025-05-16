fbbfebb1f6d4009aede0eac420d7077ff98f3b69
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-05-16 21:14:24 +0900

    2025-05-16_변영준

---
+++ 2025-05-Week2/2025-05-16_upper.java
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181877?language=java
+
+class Solution {
+    public String solution(String myString) {
+        return myString.toUpperCase();
+    }
+}
