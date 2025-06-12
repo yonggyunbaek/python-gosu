e634df9eca0ce8cc4e12fcab38fee440281490af
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-06-12 21:07:55 +0900

    2025-06-12_변영준

---
+++ 2025-06-Week2/2025-06-12_substring2.java
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181911
+
+class Solution {
+    public String solution(String[] my_strings, int[][] parts) {
+        String answer = "";
+        
+        for(int i=0; i<my_strings.length; i++){
+            answer += my_strings[i].substring(parts[i][0],parts[i][1]+1);
+        }
+        return answer;
+    }
+}
