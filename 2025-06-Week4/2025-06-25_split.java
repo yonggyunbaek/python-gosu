7fc6aa90af2377bd637cb0aae49f78330ef54f1b
Youngjun <76712506+byj1009@users.noreply.github.com>
2025-06-25 20:51:53 +0900

    2025-06-25_변영준

---
+++ 2025-06-Week4/2025-06-25_split.java
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181945
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = sc.next().split("");
+        for(String s:str){
+            System.out.println(s);
+        }
+
+    }
+}
